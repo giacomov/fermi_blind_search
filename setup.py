d63e2166d98471314cead4f70c31f88267aaacdb
giacomov <giacomo.vianello@gmail.com>
2017-02-13 16:02:43 -0800

    Initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='fermi_blind_search',
+    version='1.0',
+    packages=['scripts', 'fermi_blind_search', 'fermi_blind_search.bkge', 'fermi_blind_search.batch',
+              'fermi_blind_search.grid_creation'],
+    author='giacomov',
+    author_email='giacomov@stanford.edu',
+    description=''
+)
