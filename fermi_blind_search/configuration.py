055b1bdba785fbf6eb52f128ec11f80e98c9daa4
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-07-17 13:42:30 -0700

    Removing the use of the environment variable for the config script

--- fermi_blind_search/configuration.py
+++ fermi_blind_search/configuration.py
@@ -3,16 +3,30 @@ import ConfigParser
 import os
 
 
+_configuration = None
+
+
 def get_config(config_file=None):
+
+    global _configuration
+
+    if _configuration is not None:
+
+        return _configuration
+
+    # If we are here the user need to have passed a configuration file in input
+    assert config_file is not None, "This is the first time the configuration is built, " \
+                                    "but no file was received as input"
+
     configuration = ConfigParser.SafeConfigParser()
 
-    if config_file is not None:
-        print("SETTING CONFIG FILE")
-        os.environ['LTF_CONFIG_FILE'] = config_file
+    # if config_file is not None:
+    #     print("SETTING CONFIG FILE")
+    #     os.environ['LTF_CONFIG_FILE'] = config_file
 
-    assert os.environ.get("LTF_CONFIG_FILE") is not None, "You have to set up the LTF_CONFIG_FILE env. variable"
+    # assert os.environ.get("LTF_CONFIG_FILE") is not None, "You have to set up the LTF_CONFIG_FILE env. variable"
 
-    confPath = os.path.abspath(os.path.expandvars(os.path.expanduser(os.environ.get("LTF_CONFIG_FILE"))))
+    confPath = os.path.abspath(os.path.expandvars(os.path.expanduser(config_file)))
 
     assert os.path.exists(confPath), "Configuration path %s does not exist! " % confPath
 
@@ -21,7 +35,9 @@ def get_config(config_file=None):
     # Monkey patch adding the filename
     configuration.config_file = confPath
 
-    return configuration
+    _configuration = configuration
+
+    return _configuration
 
 
 # Read the configuration in module variables,
