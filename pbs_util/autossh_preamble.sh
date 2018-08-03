395ffd60bf51559bea74b1b726ce3833fd1c9692
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-08-03 11:54:28 -0700

    Changing monitoring port for autossh

--- pbs_util/autossh_preamble.sh
+++ pbs_util/autossh_preamble.sh
@@ -6,10 +6,10 @@
 #when the port forwarding is already active
 export AUTOSSH_MAXLIFETIME=86400
 export AUTOSSH_POLL=10
-/storage/applications/autossh/build-1.4c/bin/autossh -f -M 5000 -L 3306:localhost:3306 -N galprop-cluster.stanford.edu
+/home/ltf_blind/autossh/build-1.4f/bin/autossh -f -M 5123 -L 3306:localhost:3306 -N galprop-cluster.stanford.edu || echo "Couldn't open MySQL tunnel. Maybe already open?"
 
 #Setup SMTP port forwarding.
-/storage/applications/autossh/build-1.4c/bin/autossh -f -M 5000 -L 65530:localhost:25 -N galprop-cluster.stanford.edu
+/home/ltf_blind/autossh/build-1.4f/bin/autossh -f -M 5124 -L 65530:localhost:25 -N galprop-cluster.stanford.edu || echo "Couldn't open SMTP tunnel. Maybe already open?"
 
 
 exit 0
